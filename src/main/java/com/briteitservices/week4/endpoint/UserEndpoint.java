50a828c3e5ee7c0cecb83afd4efb2eaf674b65b5
frank <frank@britensw.com.au>
2017-12-02 14:53:17 +1100

    Update

--- src/main/java/com/briteitservices/week4/endpoint/UserEndpoint.java
+++ src/main/java/com/briteitservices/week4/endpoint/UserEndpoint.java
@@ -10,6 +10,7 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -29,5 +30,13 @@ public class UserEndpoint {
     public SerializedObject<List<User>> get() {
         List<User> users = userRepository.findAll();
         return VIEW.serializeList(users);
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public SerializedObject<User> get(@PathParam("id") int id) {
+        User user = userRepository.findBy(id);
+        return VIEW.serialize(user);
     }
 }
--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -2,8 +2,13 @@ angular.module('myApp', [
     'ngResource'
 ]).factory('UserResource', ['$resource',
     function ($resource) {
-        return $resource('rest/user', {}, {});
+        return $resource('rest/user/:id', {}, {});
     }])
     .controller('myController', ['$scope', 'UserResource', function ($scope, resource) {
         $scope.users = resource.query();
+    }])
+    .controller('DemoController', ['$scope', 'UserResource', function ($scope, resource) {
+        $scope.user = resource.get({
+            "id": 1
+        });
     }]);
